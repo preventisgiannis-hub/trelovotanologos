04bca7f97194b73feec590e4497186f08ca80fca
preventisgiannis-hub <preventisgiannis@gmail.com>
2026-06-07 17:53:06 +0300

    Create herbs_data.js

---
+++ herbs_data.js
@@ -0,0 +1,48 @@
+const herbs = [
+
+{
+id:1,
+name:"Χαμομήλι",
+scientific:"Matricaria chamomilla",
+uses:"Ηρεμιστικό, στομάχι, ύπνος",
+preparation:"Έγχυμα",
+warning:"Αποφυγή σε αλλεργία στα Asteraceae"
+},
+
+{
+id:2,
+name:"Μέντα",
+scientific:"Mentha piperita",
+uses:"Δυσπεψία, φούσκωμα",
+preparation:"Έγχυμα",
+warning:"Με μέτρο"
+},
+
+{
+id:3,
+name:"Φασκόμηλο",
+scientific:"Salvia officinalis",
+uses:"Πονόλαιμος, βήχας",
+preparation:"Έγχυμα",
+warning:"Με μέτρο"
+},
+
+{
+id:4,
+name:"Λεβάντα",
+scientific:"Lavandula angustifolia",
+uses:"Άγχος, αϋπνία",
+preparation:"Έγχυμα",
+warning:"Με μέτρο"
+},
+
+{
+id:5,
+name:"Τσάι του Βουνού",
+scientific:"Sideritis scardica",
+uses:"Κρυολόγημα, ανοσοποιητικό",
+preparation:"Έγχυμα",
+warning:"Καμία γνωστή"
+}
+
+];

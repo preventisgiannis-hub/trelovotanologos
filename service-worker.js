b379d2f62a8a5ac25ddf6e5c23b628ce4bf11f6c
preventisgiannis-hub <preventisgiannis@gmail.com>
2026-06-07 18:02:57 +0300

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,9 @@
+const CACHE_NAME = "trelovotanologos-v1";
+
+self.addEventListener("install", event => {
+console.log("Service Worker εγκαταστάθηκε");
+});
+
+self.addEventListener("fetch", event => {
+
+});
